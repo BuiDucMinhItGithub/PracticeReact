c20c4951392503fa6810a348683be84fe7134a03
BuiMinh_11 <buiducminh111120@gmail.com>
2023-03-12 22:28:13 +0700

    practice useEffect

--- practice/src/components/effect/PracticeEffect.jsx
+++ practice/src/components/effect/PracticeEffect.jsx
@@ -6,67 +6,176 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
 function PracticeEffect() {
-    function newPerson(id, name, email) {
-        this.name = name;
-        this.email = email;
-        this.id = id;
-    }
+    const [value, setValue] = useState(true);
 
+    // state teacher
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [selectedProduct, setSelectedProduct] = useState();
     const [people, setPeople] = useState([]);
-    const [value, setValue] = useState(true);
+    
+    //state student
+    const [studentName, setStudentName] = useState("");
+    const [studentEmail, setStudentEmail] = useState("");
+    const [selectedStudent, setSelectedStudent] = useState();
+    const [students, setStudents] = useState([]);
+    const [studentList, setStudentList] = useState([]);
+
+
+
+    function newPerson(id, name, email, students) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+        this.students = students;
+    }
+
+    function newStudent(id, name, email, teacherId, teacherName) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.teacherId = teacherId;
+        this.teacherName = teacherName; 
+    }
+
 
     const handleClick = () => {
         setPeople(prev => {
-            const newOne = new newPerson((Math.random() * 100) + 1,  name, email);
+            const newOne = new newPerson(Math.floor(Math.random() * 10001),  name, email, null);
             const newPeopleList = [...prev,newOne];
             return newPeopleList;
         })
     };
 
     const handleEdit = () => {
+        setValue(false);
         const array = people;
         array.map((key, index) => {
             console.log(key);
             const indexOfUpdate = array.findIndex(key => key.id === selectedProduct.id);
             array[indexOfUpdate].name = name;
             array[indexOfUpdate].email = email;
+            const studentListOfUpdate = array[indexOfUpdate].students;
+            studentListOfUpdate.map((key, index) => {
+                studentListOfUpdate[index].teacherName = name;
+            })
             setPeople(array.filter(item => item.id !== 0));
-
         })
+    }
+
+    const handleRemoveAll = () => {
+        setValue(false);
+        setPeople([]);
+        setStudents([]);
     }
 
     const handleRemove = () => {
         const array = people;
+        const array2 = students;
+        setStudents(array2.filter(item => item.teacherId !== selectedProduct.id ));
         setPeople(array.filter(item => item.id != selectedProduct.id));
     };
 
     const handlePick = (value) => {
+        setStudentList(students);
+        const array = students;
         setValue(true);
-        const newOne =  new newPerson(value.id,  value.name, value.email);
+        const newOne =  new newPerson(value.id,  value.name, value.email, value.students);
         setSelectedProduct(newOne);
         setEmail(newOne.email);
         setName(newOne.name);
+        console.log(newOne);
+        if(value.id === null){
+            setStudents(array.filter(item => item.teacherId !== selectedProduct.id ));
+        } else {
+            setStudents(studentList);
+        }
+    }
+
+    const handleAddStudent = () => {
+        const array = people;
+        setStudents(prev => {
+            const newOne = new newStudent(Math.floor(Math.random() * 10001),  studentName, studentEmail, selectedProduct.id, name);
+            const newStudentList = [...prev,newOne];
+            array.map((key, index) => {
+                const indexOfUpdate = array.findIndex(key => key.id === selectedProduct.id);
+                array[indexOfUpdate].students = newStudentList;
+            })
+            return newStudentList;
+        })
+    }
+
+    const handleEditStudent = () => {
+        const array = students;
+        array.map((key, index) => {
+            console.log(key);
+            const indexOfUpdate = array.findIndex(key => key.id === selectedStudent.id);
+            array[indexOfUpdate].name = studentName;
+            array[indexOfUpdate].email = studentEmail;
+            setStudents(array.filter(item => item.id !== 0));
+        })
+    }
+
+    const handleRemoveStudent = () => {
+        const array = students;
+        setStudents(array.filter(item => item.id != selectedStudent.id));
+    }
+
+    const handlePickStudent = (value) => {
+        console.log(selectedStudent);
+        const newOne =  new newStudent(value.id,  value.name, value.email, value.teacherName);
+        setSelectedStudent(newOne);
+        setStudentEmail(newOne.email);
+        setStudentName(newOne.name);
     }
 
     useEffect(() => {
-    }, [people])
+        setValue(true);
+    }, [people, students])
     
   return (
     <div style={{margin:'20px'}}>
-        <Button onClick={handleClick} label="Submit" style={{marginRight:'20px'}}/>
-        <Button onClick={handleRemove} label="Remove" style={{marginRight:'20px'}}/>
-        <Button onClick={handleEdit} label="Edit" style={{marginRight:'20px'}}/>
-        <InputText name='name' value={value === true ? name : ""} placeholder='Name' type="text"  onChange={e => setName(e.target.value)} style={{marginRight:'20px'}}/>
-        <InputText name='email' value={value === true ? email : ""} placeholder='Email' type="text"  onChange={e => setEmail(e.target.value)} />
-        <DataTable value={people} selectionMode="single" selection={selectedProduct}
-        onSelectionChange={(e) => handlePick(e.value)} dataKey="id" style={{marginTop:'20px'}}>
-            <Column field="id" header="Code"></Column>
-            <Column field="name" header="Name"></Column>
-            <Column field="email" header="Email"></Column>
-        </DataTable>
+        <div className='row'>
+            <div className='col-lg-3'>
+                <div>
+                    <InputText name='name' value={value === true ? name : ""} placeholder='Name' type="text"  onChange={e => setName(e.target.value)} style={{marginRight:'20px'}}/>
+                    <InputText name='email' value={value === true ? email : ""} placeholder='Email' type="text"  onChange={e => setEmail(e.target.value)} />
+                </div>
+                <div style={{marginTop: '20px'}}>
+                    <Button onClick={handleClick} label="Submit" style={{marginRight:'20px'}}/>
+                    <Button onClick={handleRemove} label="Remove" style={{marginRight:'20px'}}/>
+                    <Button onClick={handleEdit} label="Edit" style={{marginRight:'20px'}}/>
+                    <Button onClick={handleRemoveAll} label="Remove All" style={{marginRight:'20px'}}/>
+                </div>    
+            </div>
+            <div className='col-lg-4'>
+                <DataTable value={people} selectionMode="single" selection={selectedProduct}
+                    onSelectionChange={(e) => handlePick(e.value)} dataKey="id">
+                    <Column field="id" header="Code"></Column>
+                    <Column field="name" header="Name"></Column>
+                    <Column field="email" header="Email"></Column>
+                </DataTable>
+            </div>
+            <div className='col-lg-5'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <div>
+                            <InputText name='name' value={value === true ? studentName : ""} placeholder='Student name' type="text"  onChange={e => setStudentName(e.target.value)} style={{marginRight:'20px'}}/>
+                            <InputText name='email' value={value === true ? studentEmail : ""} placeholder='Student email' type="text"  onChange={e => setStudentEmail(e.target.value)} style={{marginRight:'20px'}}/><Button onClick={handleAddStudent} label="Add" style={{marginRight:'20px'}}/>
+                            <Button onClick={handleRemoveStudent} label="Remove" style={{marginRight:'20px'}}/>
+                            <Button onClick={handleEditStudent} label="Edit" style={{marginRight:'20px'}}/>
+                        </div>
+                    </div>
+                </div>
+                <DataTable value={students} selectionMode="single" selection={selectedStudent} style={{marginTop: '20px'}}
+                    onSelectionChange={(e) => handlePickStudent(e.value)} dataKey="id">
+                    <Column field="id" header="Code"></Column>
+                    <Column field="name" header="Student name"></Column>
+                    <Column field="email" header="Student email"></Column>
+                    <Column field="teacherName" header="Teacher"></Column>
+                </DataTable>
+            </div>
+        </div>
     </div>
   )
 }
--- practice/src/index.js
+++ practice/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "primereact/resources/themes/lara-light-indigo/theme.css";     
-    
-//core
-import "primereact/resources/primereact.min.css";
 
-//icons
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";  
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
